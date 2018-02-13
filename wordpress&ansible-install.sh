86d039df8bceb538790cc130a9188af7d769d527
Hibath98 <32664269+Hibath98@users.noreply.github.com>
2018-02-13 10:14:57 +0100

    Create wordpress&ansible-install.sh

---
+++ wordpress&ansible-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+git clone https://github.com/andreipak/wordpress-ansible.git
+cd ~/wordpress-ansible/
+sudo ansible-playbook -i ./hosts playbook.yml
